aa34337e8781f08d97a0dbe3318952f261bbf201
Khalisah Khan <khalisahkhan10@icloud.com>
2024-05-16 21:36:27 -0400

    w1d1 homework

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,22 +1,19 @@
 def calculate_average_grade():
-    # Prompt the user for their name and store it in the student_name variable
-    student_name = ""
+    student_name =input("Please enter your name: ")
+    print(f"Student: {student_name}")
 
-    # Prompt the user for their scores in Math, Science, and English
-    # Store the scores in the respective variables: math_score, science_score, english_score
-    math_score = 0
-    science_score = 0
-    english_score = 0
+    math_score = float(input("Enter math score: "))
+    print(f"Math: {math_score}")
+    science_score = float(input("Enter science score: "))
+    print(f"Science: {science_score}")
+    english_score = float(input("Enter English score: "))
+    print(f"English: {english_score}")
 
-    # Calculate the average grade
-    average_grade = 0
-
-    # Return the student's name and their average grade
+    average_grade = float((math_score + science_score + english_score) / 3.0)
+    print("Average Grade:", average_grade)
     return student_name, average_grade
 
 if __name__ == '__main__':
-    # Call the calculate_average_grade function
     student_name, average_grade = calculate_average_grade()
 
-    # Print the student's name and their average grade
-    print(f"")
+    print(f"{student_name}'s average grade is {average_grade:.2f}.")
